aa8802e08ce1f6ee1597d4fcaf141762dbcb6a7e
Eduardo Gonçalves <eduardouc388@gmail.com>
2025-08-18 09:55:42 -0300

    Update EstudoMySql.sql

--- EstudoMySql.sql
+++ EstudoMySql.sql
@@ -2,9 +2,14 @@ CREATE DATABASE Aula0;
 
 USE Aula0;
 
-CREATE table teste (
-	ID int primary key,
-    NOME varchar(100) not null
+CREATE table Aula0.Pessoa (
+	ID int primary key auto_increment,
+    NOME varchar(255) not null,
+    cpf varchar(11) unique not null,
+    data_nasc date not null,
+    saldo float not null default 0.0
 );
 
-DROP database aula0;
+-- DROP DATABASE aula0; Apaga o banco de dados
+
+DROP table myTable;
